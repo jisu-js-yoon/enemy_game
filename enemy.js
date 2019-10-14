c299c73d850cf712f17e7c76a2bb937a9435c6cf
Jisu <dbswltn111@gmail.com>
2019-10-14 17:21:14 +0900

    'enemy_rain_game'

---
+++ enemy.js
@@ -0,0 +1,65 @@
+class Enemy {
+    constructor() {
+        this.width = 45;
+        this.height = 54;
+        this.enemyCreate();
+        this.interval();
+        this.clearInterval();
+    }
+
+    // init() {
+    //
+    // }
+
+    enemyCreate(){
+        this.div = document.createElement('div');
+        this.div.className = "character_enemy";
+        this.div.style.postion="absolute";
+        this.div.style.top = '0px';
+        this.div.style.left = Math.floor(Math.random() * (800-this.height))+'px';
+        document.getElementById('bg').appendChild(this.div);
+    }
+
+    enemyFalling(){
+        this.yValue = parseInt(this.div.style.top);
+        this.yValue += 15;
+        this.div.style.top = this.yValue + "px";
+    }
+
+    enemyDying(){
+        this.xValue = parseInt(this.div.style.left);
+        if(this.yValue >= 530 && ((this.xValue -27.5 <= (parseInt(current_hero))) && (this.xValue + 27.5 >= (parseInt(current_hero))))){
+            this.div.style.backgroundPosition = '-45px 0';
+        }
+    }
+
+
+    interval(){
+        //setinterval(this.enemyFalling(),200);
+
+        //es5
+        // setInterval(function() {
+        //     // es5 에서 function 내의 this는 window 객체를 가르키기 때문에 안된다.
+        //     this.enemyFalling();
+        // }, 200);
+
+
+        //es6
+        setInterval(() => {
+            //this.enemyCreate();
+            this.enemyFalling();
+            this.enemyDying();
+        }, 200)
+    }
+
+    clearInterval(){
+        if(this.yValue > 527) {
+            clearInterval();
+        }
+    }
+
+
+}
+
+enemy = new Enemy();
+//enemy.init();
